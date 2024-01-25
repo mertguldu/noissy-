7c9a72d0e269ce16750859a8f85a4cb7e558cae5
B. Mert Guldu <mertguldu00@gmail.com>
2024-01-26 00:04:35 +0300

    Initial Commit

---
+++ noissy/noissyApp.swift
@@ -0,0 +1,17 @@
+//
+//  noissyApp.swift
+//  noissy
+//
+//  Created by Mert Guldu on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct noissyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
